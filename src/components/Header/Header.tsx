0de511a402073b0144334afd68808c5ebfc0120d
Trevor Leeman <Tlemurz@gmail.com>
2022-11-19 18:54:38 -0800

    Update to next 13

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,32 +2,25 @@ import Link from 'next/link';
 
 import { Text } from '@nextui-org/react';
 
+import useTailwindMinBreakpoint from '../../hooks/useTailwindBreakpoint';
 import HomeIcon from '../Icons/Home';
 import ItemTable from '../ItemTable/ItemTable';
 import Search from '../Search/Search';
 import ThemeToggle from '../ThemeToggle/ThemeToggle';
 
 const Header = () => {
+  const isMinTablet = useTailwindMinBreakpoint('sm');
+
   return (
     <header className="flex items-center gap-5 p-4 sm:grid sm:grid-cols-3">
-      <Link href="/">
-        <a>
-          <div className="hidden sm:block">
-            <Text
-              css={{
-                textGradient: '45deg, $blue600 -20%, $pink600 50%',
-                fontWeight: 'bold',
-                fontSize: '1.5rem',
-                cursor: 'pointer',
-              }}
-            >
-              OSRS Exchange
-            </Text>
-          </div>
-          <div className="sm:hidden">
-            <HomeIcon />
-          </div>
-        </a>
+      <Link href="/" title="OSRS Exchange Homepage">
+        {isMinTablet ? (
+          <span className="bg-gradient-to-b from-yellow-400 to-yellow-500 bg-clip-text text-2xl font-bold text-transparent">
+            OSRS Exchange
+          </span>
+        ) : (
+          <HomeIcon />
+        )}
       </Link>
       <div className="flex grow justify-center">
         <Search />
