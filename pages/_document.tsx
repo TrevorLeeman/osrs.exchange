552b71e89bc9d1a63a98c3a3bd96c813725abdde
Trevor Leeman <Tlemurz@gmail.com>
2022-12-04 06:57:16 -0800

    Add google analytics

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import Document, { DocumentContext, Head, Html, Main, NextScript } from 'next/document';
+import Script from 'next/script';
 
 const DESCRIPTION =
   'Profit off the the GE using live prices, kept up to date every 30 seconds via the OSRS Wiki Real-time Prices API.';
@@ -32,6 +33,25 @@ class MyDocument extends Document {
           <link
             href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&display=swap"
             rel="stylesheet"
+          />
+          {/* Google tag (gtag.js) */}
+          <Script
+            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+            strategy="afterInteractive"
+          />
+          <Script
+            id="google-analytics"
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+                page_path: window.location.pathname,
+                });
+              `,
+            }}
           />
         </Head>
         <body>
