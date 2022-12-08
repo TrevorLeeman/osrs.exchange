3f028e3e7d763fdf9d74b52c21451cb2783ba0a8
Trevor Leeman <Tlemurz@gmail.com>
2022-12-08 02:20:05 -0800

    Remove unneccessary theme color

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -22,7 +22,6 @@ class MyDocument extends Document {
           <meta name="description" content={DESCRIPTION} />
           <meta property="og:description" content={DESCRIPTION} />
           <meta name="msapplication-config" content="browserconfig.xml" />
-          <meta name="theme-color" content="#ffffff" />
           <link rel="apple-touch-icon" sizes="180x180" href="apple-touch-icon.png" />
           <link rel="icon" type="image/png" sizes="32x32" href="favicon-32x32.png" />
           <link rel="icon" type="image/png" sizes="16x16" href="favicon-16x16.png" />
