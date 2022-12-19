6a9315861694350bd2ceeb2193cf1ef522e09214
Trevor Leeman <Tlemurz@gmail.com>
2022-12-18 19:58:30 -0800

    Add horizontal padding to 404 page

--- src/components/404/404.tsx
+++ src/components/404/404.tsx
@@ -2,6 +2,7 @@ import Head from 'next/head';
 import Image from 'next/image';
 
 import H1 from '../Common/H1';
+import HorizontalPadding from '../Common/HorizontalPadding';
 import LinkBlue from '../Common/LinkBlue';
 import PageDescription from '../Common/PageDescription';
 
@@ -11,15 +12,17 @@ const NotFound = () => (
       <title>Item Not Found - OSRS Exchange</title>
       <meta name="robots" content="noindex" />
     </Head>
-    <div className="flex w-full flex-col items-center justify-center gap-5 px-3">
-      <div className="flex flex-col gap-2">
-        <H1>Item Not Found</H1>
-        <PageDescription>
-          Search for another item, or <LinkBlue href="/">return to the homepage</LinkBlue>.
-        </PageDescription>
+    <HorizontalPadding>
+      <div className="flex w-full flex-col items-center justify-center gap-5">
+        <div className="flex flex-col gap-2">
+          <H1>Item Not Found</H1>
+          <PageDescription>
+            Search for another item, or <LinkBlue href="/">return to the homepage</LinkBlue>.
+          </PageDescription>
+        </div>
+        <Image src={'/item-not-found.gif'} width={384} height={384} alt="Nieve starring into the distant sun" />
       </div>
-      <Image src={'/item-not-found.gif'} width={384} height={384} alt="Nieve starring into the distant sun" />
-    </div>
+    </HorizontalPadding>
   </>
 );
 
