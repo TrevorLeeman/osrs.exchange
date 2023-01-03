47f952f63c52249cf069194e1755f873ba0c62e2
Trevor Leeman <Tlemurz@gmail.com>
2023-01-03 01:54:36 -0800

    Render pagination component only on client (prevent hydration errors)

--- src/components/ItemTable/TablePagination.tsx
+++ src/components/ItemTable/TablePagination.tsx
@@ -1,13 +1,15 @@
 import { Pagination } from '@nextui-org/react';
+import { useIsClient } from 'usehooks-ts';
 
 import { useItemTableContext } from '../../hooks/useItemTableContext';
 import useTailwindMinBreakpoint from '../../hooks/useTailwindBreakpoint';
 
 const TablePagination = () => {
+  const isClient = useIsClient();
   const { table, setPageIndex } = useItemTableContext();
   const isMaxTablet = !useTailwindMinBreakpoint('sm');
 
-  return (
+  return isClient ? (
     <Pagination
       total={table.getPageCount()}
       onChange={pageIndex => setPageIndex(pageIndex - 1)}
@@ -16,7 +18,7 @@ const TablePagination = () => {
       noMargin={isMaxTablet}
       size={isMaxTablet ? 'md' : 'lg'}
     />
-  );
+  ) : null;
 };
 
 export default TablePagination;
